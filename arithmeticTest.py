34499ab51d05ec274ceda1817557bc6765ad52aa
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-06 14:50:33 +0000

    Update arithmeticTest.py

--- arithmeticTest.py
+++ arithmeticTest.py
@@ -2,10 +2,31 @@
 import random
 
 name = input("What's your name? ")
+operators = ["-", "+", "×", "÷"]
 
-operators = ["-". "+", "×", "÷"]
+x = int()
 
-first = random.randrange(100)
-second = random.rangerange(100)
-input(first, second)
+for _ in range(10):
+    first = random.randint(1, 20)
+    print(first)
+    second = random.randint(2, first)
+    operator = random.choice(operators)
+    message = f"{str(first)} {operator} {str(second)} = "
+    answer = input(message)
 
+    match operator:
+        case "-":
+            correctAnswer = first - second
+        case "+":
+            correctAnswer = first + second
+        case "×":
+            correctAnswer = first * second
+        case "÷":
+            correctAnswer = first / second
+
+    if answer == str(correctAnswer):
+        print("Correct answer! ")
+        x += 1
+    else:
+        print("Wrong answer :( ")
+print(x,"/10")
