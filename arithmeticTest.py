fdb3bfbb60562325d7d36bdef7b107b1b37101ba
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-06 12:28:11 +0100

    DSADSA

---
+++ arithmeticTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+
+name = input("What's your name? ")
+
+operators = ["-". "+", "×", "÷"]
+
+first = random.randrange(100)
+second = random.rangerange(100)
+input(first, second)
+
