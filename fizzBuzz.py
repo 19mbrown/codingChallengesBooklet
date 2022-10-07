603b546d6e05cfab552f0b03615d002d834d6d83
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-07 20:21:09 +0000

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,15 +1,26 @@
 x = int(input("Enter an integer: "))
+fizz = int(input("Fizz: "))
+buzz = int(input("Buzz: "))
+
+def isprime(num):
+    for n in range(2,int(num**0.5)+1): 
+        if num%n==0: return False
+    return True
 
 for i in range(1, x):
     x = False
-    if i % 3 == 0 and not i % 5 == 0:
+    if i % fizz == 0 and not i % buzz == 0:
         print("Fizz")
         x = True
-    if i % 5 == 0 and not i % 3 == 0:
+    if i % fizz == 0 and not i % buzz == 0:
         print("Buzz")
         x = True
-    if i % 3 == 0 and i % 5 == 0:
+    if i % fizz == 0 and i % buzz == 0:
         print("FizzBuzz")
+        x = True
+    if isprime(i):
+        print("OOPS")
         x = True
     if not x:
         print(i)
+
