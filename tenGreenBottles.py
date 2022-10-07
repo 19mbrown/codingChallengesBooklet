adaa02332bb8aa1309dc151f23e4b95d35386b1b
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-07 20:27:05 +0000

    Create tenGreenBottles.py

---
+++ tenGreenBottles.py
@@ -0,0 +1,2 @@
+for i in reversed(range(1, 10 + 1)): print(f"{i} green bottles hanging on the wall,\n{i} green bottles hanging on the wall,\nAnd if one green bottle should accidentally fall,\nThere'll be {i-1} green bottles hanging on the wall.")
+## 1 line :)
