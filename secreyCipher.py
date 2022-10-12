e618eb914a74934d8730575fd696886b9b679121
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-12 17:38:14 +0000

    Create secreyCipher.py

---
+++ secreyCipher.py
@@ -0,0 +1,61 @@
+from string import ascii_lowercase as letters
+
+def vigenere(text, key):
+    text, key = text.lower(), key.lower()
+    keystream = (key * len(text))[:len(text)]
+    matrix = {
+            'a':['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+            'b':['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a'],
+            'c':['c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b'],
+            'd':['d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c'],
+            'e':['e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d'],
+            'f':['f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e'],
+            'g':['g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f'],
+            'h':['h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g'],
+            'i':['i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h'],
+            'j':['j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i'],
+            'k':['k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j'],
+            'l':['l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k'],
+            'm':['m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l'],
+            'n':['n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m'],
+            'o':['o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n'],
+            'p':['p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'],
+            'q':['q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'],
+            'r':['r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'],
+            's':['s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'],
+            't':['t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'],
+            'u':['u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'],
+            'v':['v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u'],
+            'w':['w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v'],
+            'x':['x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w'],
+            'y':['y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'],
+            'z':['z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y']
+        }
+    
+    crypt = str()
+    for i in zip(text, keystream):
+        if i[0] in letters and i[1] in letters:
+            tmp = letters.index(i[1])
+            crypt += matrix[i[0]][tmp]
+        elif i[0] == " ":
+            crypt += " "
+    return crypt
+
+def ceaser(text, key):
+    text = text.lower()
+    key = int(key)
+    letterShift = letters[key:] + letters[:key]
+    crypt = str()
+    for i in text:
+        crypt += letterShift[letters.index(i)]
+    return crypt
+
+def vernam(text, key):
+    crypt = str()
+    p = 0
+    for char in text:
+        crypt += chr(ord(char) ^ ord(key[p]))
+        p += 1
+        if p==len(key):
+            p = 0
+    return crypt
