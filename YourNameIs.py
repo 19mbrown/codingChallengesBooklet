577f5bffee24a27effe7e4572c07ffb9623fe051
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-12 20:43:40 +0000

    Create YourNameIs.py

---
+++ YourNameIs.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+form = input("What for are you in? ")
+age = input("How old are you? ")
+
+print(f"Your name is {name}, you are {age} years old, and you are in form {form}.")
